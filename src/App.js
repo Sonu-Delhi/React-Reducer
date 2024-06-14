c752b867e69d4a320a26ae052f1e6dcf187da838
Sonu-Delhi <sy9812672@gmail.com>
2024-06-14 21:41:37 +0530

    React Reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, {useReducer} from 'react';
 import './App.css';
 
+const redcureComponent = (state,action)=>{
+  console.log(state);
+  console.log(action);
+  if(action.type==="increment"){
+    return state+1;
+  }
+  if(action.type==="decrement"){
+    return state-1;
+  }
+
+  return state;
+}
 function App() {
+ let [counter, dispatch] =  useReducer(redcureComponent,10);
+
+  const increment = ()=>{
+    
+    dispatch({
+      action:"incement",
+      payload:0
+    })
+  }
+  const decrement = ()=>{
+    
+    dispatch({
+      action:"decrement",
+      payload:0
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>{counter}</p>
+     <div>
+      <button onClick={()=>dispatch({type:"increment"})} className='btn btn-primary'>Increment</button>
+      <button onClick={()=>dispatch({type:"decrement"})} className='btn btn-warning mx-2'>Decrement</button>
+     </div>
     </div>
   );
 }
